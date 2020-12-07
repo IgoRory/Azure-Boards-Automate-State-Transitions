31fbe8def1624714677275b40aceaccc60d2e2da
IgoRory <75446925+IgoRory@users.noreply.github.com>
2020-12-07 14:08:17 -0500

    Update Startup.cs

--- src/AutoStateTransitions/Startup.cs
+++ src/AutoStateTransitions/Startup.cs
@@ -33,8 +33,8 @@ namespace AutoStateTransitions
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
-
+            //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0).AddNewtonsoftJson();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Azure DevOps - Automate State Transitions", Version = "v1" });
